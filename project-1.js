80381e8e7015a02a4e38b17db04b20e2d250daaf
Abdul-Rehman001 <139611482+Abdul-Rehman001@users.noreply.github.com>
2024-02-22 12:27:56 +0530

    Update project-1.js

--- project-1.js
+++ project-1.js
@@ -2,6 +2,7 @@ const quote = document.querySelector('.hadith')
 const person = document.querySelector('.person')
 const btn = document.querySelector('#new-quote')
 const apiUrl = "https://random-hadith-generator.vercel.app/bukhari/";
+const refrence = document.querySelector('.refrence');
 
 async function getQuote(url){
     const response = await fetch(url);
@@ -10,6 +11,7 @@ async function getQuote(url){
     console.log(data);
     quote.innerHTML = data.data.hadith_english;
     person.innerHTML = data.data.header;
+    refrence.innerHTML = data.data.refno;
 
 }
 getQuote(apiUrl);
